f712c7f7c1dea2862b24429fb335dc4a81022a6e
Leo <leo.on.the.earth@gmail.com>
2023-01-23 20:49:34 +0800

    Add interface for generating routing name

---
+++ src/Document/RoutingEnabledDocument.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Nadia\ElasticSearchODM\Document;
+
+interface RoutingEnabledDocument
+{
+    /**
+     * Using document properties to generate routing name
+     *
+     * For example, a document has values as below:
+     * <code>
+     * {
+     *   "uid": 6867926,
+     * }
+     * </code>
+     *
+     * We want to generate a routing name "uid:6867926",
+     * the "20221201" part is generated from "uid" property,
+     * then implement "generateRoutingName" method as below:
+     *
+     * <code>
+     * public static function generateRoutingName($document)
+     * {
+     *   return 'uid:' . (is_object($document) ? $document->getUid() : $document['uid']);
+     * }
+     * </code>
+     *
+     * @param array|object $document
+     *
+     * @return string
+     */
+    public static function generateRoutingName($document);
+}
