7fe47d763bf3d267300ecead891364b1d4904628
Leo <leo.on.the.earth@gmail.com>
2023-02-11 22:14:02 +0800

    Add IndexNameProvider to provide valid index names

---
+++ src/Document/IndexNameProvider.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Nadia\ElasticSearchODM\Document;
+
+use ElasticSearch\Client;
+use Psr\Cache\CacheItemPoolInterface;
+
+class IndexNameProvider
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $indexNamePrefix;
+
+    /**
+     * @var int
+     */
+    protected $indexNamePrefixLength;
+
+    /**
+     * @var CacheItemPoolInterface|null
+     */
+    protected $cache;
+
+    /**
+     * @var bool
+     */
+    protected $refreshIndexAliasesCache = false;
+
+    /**
+     * @var string[]
+     */
+    protected $aliases;
+
+    /**
+     * IndexNameProvider constructor.
+     *
+     * @param Client $client
+     * @param string $indexNamePrefix
+     * @param CacheItemPoolInterface|null $cache
+     */
+    public function __construct($client, $indexNamePrefix = '', CacheItemPoolInterface $cache = null)
+    {
+        $this->client = $client;
+        $this->indexNamePrefix = $indexNamePrefix;
+        $this->indexNamePrefixLength = strlen($indexNamePrefix);
+        $this->cache = $cache;
+    }
+
+    /**
+     * @param string[] $indexNames Index names without prefix
+     *
+     * @return string[] Valid index names with prefix
+     *
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function getValidIndexNames(array $indexNames)
+    {
+        $this->loadValidIndexNames();
+
+        $validIndexNames = [];
+
+        foreach ($indexNames as $indexName) {
+            $indexName = $this->indexNamePrefix . $indexName;
+
+            if (false !== strpos($indexName, '*') || isset($this->aliases[$indexName])) {
+                $validIndexNames[] = $indexName;
+            }
+        }
+
+        return $validIndexNames;
+    }
+
+    /**
+     * @return static
+     *
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function loadValidIndexNames()
+    {
+        if (!$this->refreshIndexAliasesCache && !is_null($this->aliases)) {
+            return $this;
+        }
+
+        if (!$this->refreshIndexAliasesCache && $this->cache instanceof CacheItemPoolInterface) {
+            $cacheItem = $this->cache->getItem($this->getIndexAliasesCacheKey());
+
+            if ($cacheItem->isHit()) {
+                $this->aliases = (array)$cacheItem->get();
+
+                return $this;
+            }
+        }
+
+        $this->aliases = [];
+
+        foreach ($this->client->indices()->getAliases() as $key => $alias) {
+            if ($this->isValidIndexName($key)) {
+                $this->aliases[$key] = true;
+            }
+
+            if (!empty($alias['aliases']) && is_array($alias['aliases'])) {
+                foreach ($alias['aliases'] as $aliasIndexName) {
+                    if ($this->isValidIndexName($aliasIndexName)) {
+                        $this->aliases[$aliasIndexName] = true;
+                    }
+                }
+            }
+        }
+
+        if ($this->cache instanceof CacheItemPoolInterface) {
+            if (!isset($cacheItem)) {
+                $cacheItem = $this->cache->getItem($this->getIndexAliasesCacheKey());
+            }
+            $cacheItem->set($this->aliases);
+            $this->cache->save($cacheItem);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return static
+     */
+    public function enableRefreshIndexAliasesCache()
+    {
+        $this->refreshIndexAliasesCache = true;
+
+        return $this;
+    }
+
+    /**
+     * @return static
+     */
+    public function disableRefreshIndexAliasesCache()
+    {
+        $this->refreshIndexAliasesCache = false;
+
+        return $this;
+    }
+
+    public function getIndexAliasesCacheKey()
+    {
+        return 'elasticsearch_odm_client_index_aliases';
+    }
+
+    /**
+     * @param string $indexName
+     *
+     * @return bool
+     */
+    protected function isValidIndexName($indexName)
+    {
+        if (0 === $this->indexNamePrefixLength) {
+            return true;
+        }
+
+        return 0 === strncmp($indexName, $this->indexNamePrefix, $this->indexNamePrefixLength);
+    }
+}
---
+++ tests/Document/IndexNameProviderTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Nadia\ElasticSearchODM\Tests\Document;
+
+use Elasticsearch\Client;
+use Nadia\ElasticSearchODM\Document\IndexNameProvider;
+use Nadia\ElasticSearchODM\Tests\Stubs\Cache\Cache;
+use Nadia\ElasticSearchODM\Tests\Stubs\ElasticSearch\IndicesNamespace;
+use PHPUnit\Framework\TestCase;
+
+class IndexNameProviderTest extends TestCase
+{
+    public function testGetValidIndexNames()
+    {
+        $aliases = $this->getMockAliases();
+        $client = $this->getMockClient($aliases);
+        $cache = new Cache();
+        $provider = new IndexNameProvider($client, 'dev-', $cache);
+
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['dev-index-001', 'dev-index-003'], $indexNames);
+
+        // Test when IndexNameProvider::$aliases is cached
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['dev-index-001', 'dev-index-003'], $indexNames);
+
+        // Test when indexNamePrefix is empty
+        $aliases = $this->getMockAliasesWithoutIndexNamePrefix();
+        $client = $this->getMockClient($aliases);
+        $cache = new Cache();
+        $provider = new IndexNameProvider($client, '', $cache);
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['index-001', 'index-003'], $indexNames);
+    }
+
+    public function testGetValidIndexNamesWhenAliasesCacheExists()
+    {
+        $client = $this->getMockClient($this->getMockAliases());
+        $cache = new Cache();
+        $provider = new IndexNameProvider($client, 'dev-', $cache);
+
+        $cacheItem = $cache->getItem($provider->getIndexAliasesCacheKey());
+        $aliases = [];
+
+        foreach ($this->getMockAliases() as $indexName => $indexNameAliases) {
+            $aliases[$indexName] = true;
+
+            foreach ($indexNameAliases['aliases'] as $aliasIndexName) {
+                $aliases[$aliasIndexName] = true;
+            }
+        }
+        $cacheItem->set($aliases);
+        $cache->save($cacheItem);
+
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['dev-index-001', 'dev-index-003'], $indexNames);
+    }
+
+    public function testGetValidIndexNamesWithRefreshIndexAliasesCache()
+    {
+        $aliases = $this->getMockAliases();
+        $client = $this->getMockClient($aliases);
+        $cache = new Cache();
+        $provider = new IndexNameProvider($client, 'dev-', $cache);
+        $provider->enableRefreshIndexAliasesCache();
+
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['dev-index-001', 'dev-index-003'], $indexNames);
+
+        $aliases = $this->getMockAliases();
+        $client = $this->getMockClient($aliases);
+        $cache = new Cache();
+        $provider = new IndexNameProvider($client, 'dev-', $cache);
+        $provider->disableRefreshIndexAliasesCache();
+
+        $indexNames = $provider->getValidIndexNames(['index-001', 'index-003', 'index-not-exists']);
+        $this->assertEquals(['dev-index-001', 'dev-index-003'], $indexNames);
+    }
+
+    private function getMockClient($aliases)
+    {
+        $indicesNamespace = $this->getMockBuilder(IndicesNamespace::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getAliases'])
+            ->getMock();
+        $indicesNamespace->method('getAliases')->willReturn($aliases);
+
+        $client = $this->getMockBuilder(Client::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['indices'])
+            ->getMock();
+        $client->method('indices')->willReturn($indicesNamespace);
+
+        return $client;
+    }
+
+    private function getMockAliases()
+    {
+        return [
+            'dev-index-001' => ['aliases' => []],
+            'dev-index-002' => ['aliases' => []],
+            'dev-index-003' => ['aliases' => ['dev-index-003-1', 'dev-index-003-2']],
+        ];
+    }
+
+    private function getMockAliasesWithoutIndexNamePrefix()
+    {
+        return [
+            'index-001' => ['aliases' => []],
+            'index-002' => ['aliases' => []],
+            'index-003' => ['aliases' => ['index-003-1', 'index-003-2']],
+        ];
+    }
+}
